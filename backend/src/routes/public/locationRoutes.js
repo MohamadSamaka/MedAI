c477413bdf470c83b867c68796e5b4c9ee0fbf91
Mohamad Samaka <mhmdkais20@gmail.com>
2025-02-28 22:52:28 +0200

    Commit backend changes

---
+++ backend/src/routes/public/locationRoutes.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const LocationController = require("../../controllers/locationController");
+const PublicLocationRouter = Router();
+
+
+PublicLocationRouter.get("/", (req, res, next) =>
+  LocationController.getAll(req, res, next)
+);
+PublicLocationRouter.get("/:id", (req, res, next) =>
+  LocationController.getById(req, res, next)
+);
+PublicLocationRouter.get("/:id", (req, res, next) =>
+  LocationController.getClosestLocations(req, res, next)
+);
+
+PublicLocationRouter.get("/", (req, res, next) => LocationController.getAll(req, res, next));
+PublicLocationRouter.get("/closest", (res, req, next) => LocationController.getClosestLocations(req, res, next));
+
+
+
+module.exports = PublicLocationRouter;
