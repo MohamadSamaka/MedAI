35b38944a36f62be3b00e7dc2304e78593b17e73
Mohamad Samaka <mhmdkais20@gmail.com>
2025-03-01 07:32:30 +0200

    i added frontend here by mistaken , i deleted it

--- frontend/public/js/app.js
+++
@@ -1,8 +0,0 @@
-import { initRouter } from './routes/router.js';
-
-// Initialize the router when the page loads
-initRouter();
-
-window.addEventListener('popstate', () => {
-  initRouter();
-});
--- frontend/public/js/helpers/path.js
+++
@@ -1,5 +0,0 @@
-const STYLES_PATH = "/styles/"
-
-export function stylePath(fileName){
-    return `${STYLES_PATH}${fileName}`
-}
